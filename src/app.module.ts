f691a37b2b1723de2d774dedf30f1d94bdaeefaf
Luis Bueno <luisbuenodev@gmail.com>
2026-09-22 12:12:36 -0300

    set mongoDB connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    // access from any module
+    ConfigModule.forRoot({ isGlobal: true }),
+    // set mongoose to wait for ConfigModule, and then get variable to set uri
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGODB_URI'),
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
