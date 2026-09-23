ccd364d5bbb7755bb376e6435927c33a70a3e67a
Luis Bueno <luisbuenodev@gmail.com>
2026-09-23 10:16:31 -0300

    validate routes id

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -2,34 +2,37 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { ParseObjectIdPipe } from '@nestjs/mongoose';
 
 @Controller('users')
 export class UsersController {
-  
-  constructor(private readonly usersService: UsersService) {}
+	constructor(private readonly usersService: UsersService) {}
 
-  @Post()
-  create(@Body() createUserDto: CreateUserDto) {
-    return this.usersService.create(createUserDto);
-  }
+	@Post()
+	create(@Body() createUserDto: CreateUserDto) {
+		return this.usersService.create(createUserDto);
+	}
 
-  @Get()
-  findAll() {
-    return this.usersService.findAll();
-  }
+	@Get()
+	findAll() {
+		return this.usersService.findAll();
+	}
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.usersService.findOne(id);
-  }
+	@Get(':id')
+	findOne(@Param('id', ParseObjectIdPipe) id: string) {
+		return this.usersService.findOne(id);
+	}
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return this.usersService.update(id, updateUserDto);
-  }
+	@Patch(':id')
+	update(
+		@Param('id', ParseObjectIdPipe) id: string,
+		@Body() updateUserDto: UpdateUserDto,
+	) {
+		return this.usersService.update(id, updateUserDto);
+	}
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.usersService.remove(id);
-  }
+	@Delete(':id')
+	remove(@Param('id', ParseObjectIdPipe) id: string) {
+		return this.usersService.remove(id);
+	}
 }
